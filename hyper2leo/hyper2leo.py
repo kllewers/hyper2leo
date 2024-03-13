2aafb21bfa46bfb18d20648b40ebc9932ec248c7
kllewers <kristen.lewers@gmail.com>
2024-03-12 20:05:19 -0600

    updates :

--- hyper2leo/hyper2leo.py
+++ hyper2leo/hyper2leo.py
@@ -10,7 +10,9 @@ def fr_to_wv(frequency):
     wavelength = 3e8/frequency
     return wavelength
 
-def hyper_to_landsat_1_3(range_min, range_max):
+def hyper_to_landsat_mss_1_3(range_min, range_max):
+    if range_max <= 900 and range_min > 800:
+        return f"The range {range_min} - {range_max} nanometers is Band 7 (the NIR band) for Landsats 1-3"
     if range_max <= 800 and range_min > 700:
         return f"The range {range_min} - {range_max} nanometers is Band 6 (the NIR band) for Landsats 1-3"
     elif range_max <= 700 and range_min >= 600:
@@ -19,3 +21,15 @@ def hyper_to_landsat_1_3(range_min, range_max):
         return f"The range {range_min} - {range_max} nanometers is in Band 4 (the Green band) for Landsats 1-3"
     else:
         return "The range entered is outside of ranges for Landsat 1-3"
+    
+def hyper_to_landsat_mss_4_5(range_min, range_max):
+    if range_max <= 900 and range_min > 800:
+        return f"The range {range_min} - {range_max} nanometers is Band 4 (the NIR band) for Landsats 1-3"
+    if range_max <= 800 and range_min > 700:
+        return f"The range {range_min} - {range_max} nanometers is Band 3 (the NIR band) for Landsats 1-3"
+    elif range_max <= 700 and range_min >= 600:
+        return f"The range {range_min} - {range_max} nanometers is Band 2 (the Red band) for Landsats 1-3"    
+    elif range_max <= 600 and range_min >= 500:
+        return f"The range {range_min} - {range_max} nanometers is in Band 1 (the Green band) for Landsats 1-3"
+    else:
+        return "The range entered is outside of ranges for Landsat 4-5"
