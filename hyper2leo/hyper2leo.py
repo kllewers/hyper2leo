4d72ba993c104eefcca19f173f1d42ae4a9298c6
kllewers <kristen.lewers@gmail.com>
2024-03-12 19:49:37 -0600

    landsat 1-3 conversion

--- hyper2leo/hyper2leo.py
+++ hyper2leo/hyper2leo.py
@@ -10,5 +10,12 @@ def fr_to_wv(frequency):
     wavelength = 3e8/frequency
     return wavelength
 
-def hyper_to_sentinel_wavelength(range_min, range_max):
-    return range(range_min,range_max)
+def hyper_to_landsat_1_3(range_min, range_max):
+    if range_max <= 800 and range_min > 700:
+        return f"The range {range_min} - {range_max} nanometers is Band 6 (the NIR band) for Landsats 1-3"
+    elif range_max <= 700 and range_min >= 600:
+        return f"The range {range_min} - {range_max} nanometers is Band 5 (the Red band) for Landsats 1-3"    
+    elif range_max <= 600 and range_min >= 500:
+        return f"The range {range_min} - {range_max} nanometers is in Band 4 (the Green band) for Landsats 1-3"
+    else:
+        return "The range entered is outside of ranges for Landsat 1-3"
